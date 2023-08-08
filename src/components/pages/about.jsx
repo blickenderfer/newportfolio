844832a3dd0721be484a8dcf0b13f9dab1509e4d
Blickenderfer <ecblickenderfer@gmail.com>
2023-08-08 11:19:41 -0400

    added starter code for contact form

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -1,7 +1,10 @@
 export default function About(){
     return (
         <div>
-            <h1>About Page</h1>
+            <h1>About Me</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque sunt repellendus eveniet! Eveniet fugiat vitae, tempore labore optio corporis ab similique cumque odit earum! Illo error vero rem sequi ipsam labore incidunt adipisci cupiditate nostrum! Perferendis voluptatum temporibus est maxime!
+            </p>
         </div>
     )
 }
